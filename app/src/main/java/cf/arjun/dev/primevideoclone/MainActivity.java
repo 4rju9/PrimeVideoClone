315bc61c23d485f27679cf34450aa13f3dd0efe7
4rju9 <ohinarendra3550@gmail.com>
2023-12-25 22:12:28 +0530

    Setup view pager inside main activity using demo data

--- app/src/main/java/cf/arjun/dev/primevideoclone/MainActivity.java
+++ app/src/main/java/cf/arjun/dev/primevideoclone/MainActivity.java
@@ -1,15 +1,43 @@
 package cf.arjun.dev.primevideoclone;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
+import java.util.ArrayList;
+import java.util.List;
+import cf.arjun.dev.primevideoclone.adapters.BannerMoviesPagerAdapter;
+import cf.arjun.dev.primevideoclone.models.BannerMovies;
 
 public class MainActivity extends AppCompatActivity {
+
+    BannerMoviesPagerAdapter bannerMoviespagerAdapter;
+    TabLayout tabLayout;
+    ViewPager bannerMoviesViewPager;
+    List<BannerMovies> bannerMoviesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bannerMoviesList = new ArrayList<>();
+        bannerMoviesList.add(new BannerMovies(1, "AQUA MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7byybfv7B3BlEb7btSnQ2XAQRHDao9q9_Iw&usqp=CAU", ""));
+        bannerMoviesList.add(new BannerMovies(2, "BLACK WIDOW", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNyJ1uXbVY-smPvVtdaK4c8eyAimNjWxz-VA&usqp=CAU", ""));
+        bannerMoviesList.add(new BannerMovies(3, "THE AMAZING SPIDER MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3cHeuIyp7dYGjJaq351m97BBj_15zazCESA&usqp=CAU", ""));
+        bannerMoviesList.add(new BannerMovies(4, "JUSTICE LEAGUE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbB5WuyTsBdfgfKZr3YaXbRq0giaHErYkyXA&usqp=CAU", ""));
+        bannerMoviesList.add(new BannerMovies(5, "K. G. F. CHAPTER 2", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTQGHO4P6zJkavlE_PPD1Whm6aUme9JrX-Dg&usqp=CAU", ""));
+
+        setBannerMoviesPagerAdapter(bannerMoviesList);
+
     }
+
+    private void setBannerMoviesPagerAdapter (List<BannerMovies> bannerMoviesList) {
+
+        bannerMoviesViewPager = findViewById(R.id.bannerViewPager);
+        bannerMoviespagerAdapter = new BannerMoviesPagerAdapter(this, bannerMoviesList);
+        bannerMoviesViewPager.setAdapter(bannerMoviespagerAdapter);
+
+    }
+
 }
