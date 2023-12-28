6311e233a0f85f3169d1182d15f02efa5e902c94
4rju9 <ohinarendra3550@gmail.com>
2023-12-28 22:33:48 +0530

    implemented view pager inside main activity for all the categories

--- app/src/main/java/cf/arjun/dev/primevideoclone/MainActivity.java
+++ app/src/main/java/cf/arjun/dev/primevideoclone/MainActivity.java
@@ -17,49 +17,126 @@ public class MainActivity extends AppCompatActivity {
     BannerMoviesPagerAdapter bannerMoviespagerAdapter;
     TabLayout tabLayout, indicatorTabLayout;
     ViewPager bannerMoviesViewPager;
-    List<BannerMovies> bannerMoviesList;
+    List<BannerMovies> homeBannerList;
+    List<BannerMovies> tvBannerList;
+    List<BannerMovies> moviesBannerList;
+    List<BannerMovies> kidsBannerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bannerMoviesList = new ArrayList<>();
-        bannerMoviesList.add(new BannerMovies(1, "AQUA MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7byybfv7B3BlEb7btSnQ2XAQRHDao9q9_Iw&usqp=CAU", ""));
-        bannerMoviesList.add(new BannerMovies(2, "BLACK WIDOW", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNyJ1uXbVY-smPvVtdaK4c8eyAimNjWxz-VA&usqp=CAU", ""));
-        bannerMoviesList.add(new BannerMovies(3, "THE AMAZING SPIDER MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3cHeuIyp7dYGjJaq351m97BBj_15zazCESA&usqp=CAU", ""));
-        bannerMoviesList.add(new BannerMovies(4, "JUSTICE LEAGUE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbB5WuyTsBdfgfKZr3YaXbRq0giaHErYkyXA&usqp=CAU", ""));
-        bannerMoviesList.add(new BannerMovies(5, "K. G. F. CHAPTER 2", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTQGHO4P6zJkavlE_PPD1Whm6aUme9JrX-Dg&usqp=CAU", ""));
+        setupUIViews();
+        setupLists();
 
-        setBannerMoviesPagerAdapter(bannerMoviesList);
+        // default selected tab.
+        setBannerMoviesPagerAdapter(homeBannerList);
+        // on tab data changed.
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int tabId = tab.getId();
+                switch (tabId) {
+                    case 1: {
+                        setBannerMoviesPagerAdapter(tvBannerList);
+                        break;
+                    } case 2: {
+                        setBannerMoviesPagerAdapter(moviesBannerList);
+                        break;
+                    } case 3: {
+                        setBannerMoviesPagerAdapter(kidsBannerList);
+                        break;
+                    } default: {
+                        setBannerMoviesPagerAdapter(homeBannerList);
+                    }
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
 
-    private void setBannerMoviesPagerAdapter (List<BannerMovies> bannerMoviesList) {
+    private void setupUIViews () {
+
+        indicatorTabLayout = findViewById(R.id.tabIndicator);
+        tabLayout = findViewById(R.id.tabLayout);
+
+    }
+
+    private void setupLists () {
+
+        homeBannerList = new ArrayList<>();
+        homeBannerList.add(new BannerMovies(1, "AQUA MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7byybfv7B3BlEb7btSnQ2XAQRHDao9q9_Iw&usqp=CAU", ""));
+        homeBannerList.add(new BannerMovies(2, "BLACK WIDOW", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNyJ1uXbVY-smPvVtdaK4c8eyAimNjWxz-VA&usqp=CAU", ""));
+        homeBannerList.add(new BannerMovies(3, "THE AMAZING SPIDER MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3cHeuIyp7dYGjJaq351m97BBj_15zazCESA&usqp=CAU", ""));
+        homeBannerList.add(new BannerMovies(4, "JUSTICE LEAGUE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbB5WuyTsBdfgfKZr3YaXbRq0giaHErYkyXA&usqp=CAU", ""));
+        homeBannerList.add(new BannerMovies(5, "K. G. F. CHAPTER 2", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTQGHO4P6zJkavlE_PPD1Whm6aUme9JrX-Dg&usqp=CAU", ""));
+
+        tvBannerList = new ArrayList<>();
+        tvBannerList.add(new BannerMovies(1, "AQUA MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7byybfv7B3BlEb7btSnQ2XAQRHDao9q9_Iw&usqp=CAU", ""));
+        tvBannerList.add(new BannerMovies(2, "BLACK WIDOW", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNyJ1uXbVY-smPvVtdaK4c8eyAimNjWxz-VA&usqp=CAU", ""));
+        tvBannerList.add(new BannerMovies(3, "THE AMAZING SPIDER MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3cHeuIyp7dYGjJaq351m97BBj_15zazCESA&usqp=CAU", ""));
+        tvBannerList.add(new BannerMovies(4, "JUSTICE LEAGUE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbB5WuyTsBdfgfKZr3YaXbRq0giaHErYkyXA&usqp=CAU", ""));
+        tvBannerList.add(new BannerMovies(5, "K. G. F. CHAPTER 2", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTQGHO4P6zJkavlE_PPD1Whm6aUme9JrX-Dg&usqp=CAU", ""));
+
+        moviesBannerList = new ArrayList<>();
+        moviesBannerList.add(new BannerMovies(1, "AQUA MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7byybfv7B3BlEb7btSnQ2XAQRHDao9q9_Iw&usqp=CAU", ""));
+        moviesBannerList.add(new BannerMovies(2, "BLACK WIDOW", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNyJ1uXbVY-smPvVtdaK4c8eyAimNjWxz-VA&usqp=CAU", ""));
+        moviesBannerList.add(new BannerMovies(3, "THE AMAZING SPIDER MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3cHeuIyp7dYGjJaq351m97BBj_15zazCESA&usqp=CAU", ""));
+        moviesBannerList.add(new BannerMovies(4, "JUSTICE LEAGUE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbB5WuyTsBdfgfKZr3YaXbRq0giaHErYkyXA&usqp=CAU", ""));
+        moviesBannerList.add(new BannerMovies(5, "K. G. F. CHAPTER 2", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTQGHO4P6zJkavlE_PPD1Whm6aUme9JrX-Dg&usqp=CAU", ""));
+
+        kidsBannerList = new ArrayList<>();
+        kidsBannerList.add(new BannerMovies(1, "AQUA MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7byybfv7B3BlEb7btSnQ2XAQRHDao9q9_Iw&usqp=CAU", ""));
+        kidsBannerList.add(new BannerMovies(2, "BLACK WIDOW", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNyJ1uXbVY-smPvVtdaK4c8eyAimNjWxz-VA&usqp=CAU", ""));
+        kidsBannerList.add(new BannerMovies(3, "THE AMAZING SPIDER MAN", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3cHeuIyp7dYGjJaq351m97BBj_15zazCESA&usqp=CAU", ""));
+        kidsBannerList.add(new BannerMovies(4, "JUSTICE LEAGUE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbB5WuyTsBdfgfKZr3YaXbRq0giaHErYkyXA&usqp=CAU", ""));
+        kidsBannerList.add(new BannerMovies(5, "K. G. F. CHAPTER 2", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTQGHO4P6zJkavlE_PPD1Whm6aUme9JrX-Dg&usqp=CAU", ""));
+
+    }
+
+    private void setBannerMoviesPagerAdapter (List<BannerMovies> moviesList) {
 
         bannerMoviesViewPager = findViewById(R.id.bannerViewPager);
-        bannerMoviespagerAdapter = new BannerMoviesPagerAdapter(this, bannerMoviesList);
+        bannerMoviespagerAdapter = new BannerMoviesPagerAdapter(this, moviesList);
         bannerMoviesViewPager.setAdapter(bannerMoviespagerAdapter);
-        indicatorTabLayout = findViewById(R.id.tabIndicator);
         indicatorTabLayout.setupWithViewPager(bannerMoviesViewPager, true);
 
         Timer sliderTimer = new Timer();
-        sliderTimer.scheduleAtFixedRate(new BannerAutoSlider(), 4000, 6000);
+        sliderTimer.scheduleAtFixedRate(new BannerAutoSlider(moviesList.size()), 4000, 6000);
 
     }
 
     class BannerAutoSlider extends TimerTask {
 
+        int maxSize;
+
+        public BannerAutoSlider(int maxSize) {
+            this.maxSize = maxSize;
+        }
+
         @Override
         public void run() {
 
             MainActivity.this.runOnUiThread( () -> {
+
                 int index = bannerMoviesViewPager.getCurrentItem();
-                if (index < bannerMoviesList.size() - 1) {
+                if (index < maxSize - 1) {
                     bannerMoviesViewPager.setCurrentItem(index + 1);
                 } else {
                     bannerMoviesViewPager.setCurrentItem(0);
                 }
+
             });
 
         }
